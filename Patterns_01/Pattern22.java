a772b56faade33af01d07f6857491e854e051fd8
Veekshitha V V <veekshithavv1408@hmail.com>
2025-11-27 13:37:15 +0530

    The Number Pattern

---
+++ Patterns_01/Pattern22.java
@@ -0,0 +1,41 @@
+//Pattern - 22: The Number Pattern
+package Patterns_01;
+
+import java.util.*;
+public class Pattern22 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the value of N:");
+        int N = input.nextInt();
+        print_pattern22(N);
+    }
+    public static void print_pattern22(int n){
+        for(int i=0; i< 2 * n - 1; i++){
+            for(int j=0; j< 2 * n - 1; j++){
+                int top = i;
+                int bottom = (2 * n - 2) - i;
+                int left = j;
+                int right = (2 * n - 2) - j;
+
+                int minDist = Math.min(Math.min(top,bottom) , Math.min(left,right));
+                System.out.print((n - minDist)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+//OUTPUT
+
+/*
+Enter the value of N:5
+5 5 5 5 5 5 5 5 5 
+5 4 4 4 4 4 4 4 5
+5 4 3 3 3 3 3 4 5
+5 4 3 2 2 2 3 4 5
+5 4 3 2 1 2 3 4 5
+5 4 3 2 2 2 3 4 5
+5 4 3 3 3 3 3 4 5
+5 4 4 4 4 4 4 4 5
+5 5 5 5 5 5 5 5 5
+*/
