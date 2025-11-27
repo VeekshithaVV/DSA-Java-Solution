b8568fe5752bb2b533a62eb34d0f37206c51c577
Veekshitha V V <veekshithavv1408@hmail.com>
2025-11-27 10:48:15 +0530

    Right Angled Number Pyramid

---
+++ Patterns_01/Pattern03.java
@@ -0,0 +1,30 @@
+//Pattern - 3: Right-Angled Number Pyramid
+package Patterns_01;
+
+import java.util.*;
+public class Pattern03 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the n:");
+        int n = input.nextInt();
+        print_pattern3(n);
+    }
+    public static void print_pattern3(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+//OUTPUT
+/*
+Enter the n:5
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 
+*/
