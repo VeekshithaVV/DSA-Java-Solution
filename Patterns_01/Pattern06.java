6c0485ac2fdbc7372fa9e7574a9e96ecfc7bf52e
Veekshitha V V <veekshithavv1408@hmail.com>
2025-11-27 10:57:56 +0530

    Inverted Numbered Right Pyramid

---
+++ Patterns_01/Pattern06.java
@@ -0,0 +1,30 @@
+//Pattern - 6: Inverted Numbered Right Pyramid
+package Patterns_01;
+
+import java.util.*;
+public class Pattern06 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the n:");
+        int n = input.nextInt();
+        print_pattern6(n);
+    }
+    public static void print_pattern6(int n){
+        for(int i=n; i>0; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+//OUTPUT
+/*
+Enter the n:5
+1 2 3 4 5 
+1 2 3 4 
+1 2 3 
+1 2 
+1 
+*/
