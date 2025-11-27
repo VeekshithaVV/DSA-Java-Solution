cb7a2caef99b336d0ddc9b1d456a1f36fb5348c8
Veekshitha V V <veekshithavv1408@hmail.com>
2025-11-27 11:06:05 +0530

    Diamond Star Pattern

---
+++ Patterns_01/Pattern09.java
@@ -0,0 +1,59 @@
+//Pattern - 9: Diamond Star Pattern
+package Patterns_01;
+
+import java.util.*;
+public class Pattern09 {
+     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the value of n:");
+        int n = input.nextInt();
+        print_pattern9(n);
+    }
+    public static void print_pattern9(int n){
+        for(int i=0; i<n; i++){
+            //SPACE
+            for(int j=0; j<n-i-1; j++){
+                System.out.print(" ");
+            }
+            //STAR
+            for(int j=0; j<2*i+1; j++){
+                System.out.print("*");
+            }
+            //SPACE
+            for(int j=0; j<n-i-1; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        for(int i=0; i<n; i++){
+            //SPACE
+            for(int j=0; j<i; j++){
+                System.out.print(" ");
+            }
+            //STAR
+            for(int j=0; j<2*n -(2*i+1); j++){
+                System.out.print("*");
+            }
+            //SPACE
+            for(int j=0; j<i; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+//OUTPUT
+/*
+Enter the value of n:5
+    *    
+   ***   
+  *****  
+ ******* 
+*********
+*********
+ ******* 
+  *****
+   ***
+    *
+*/
