08016ac24fcf9038425ec61443a14d3b5153789d
Veekshitha V V <veekshithavv1408@hmail.com>
2025-12-03 09:56:21 +0530

    Recursive Insertion Sort

---
+++ E_Sorting/G_Recursive_Insertion_Sort.java
@@ -0,0 +1,53 @@
+//Recursive INSERTION SORT
+package E_Sorting;
+
+import java.util.*;
+public class G_Recursive_Insertion_Sort {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the array size:");
+        int size = input.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter the array elements:");
+        for(int i=0; i<size; i++){
+            arr[i] = input.nextInt();
+        }
+        insertion_sort(arr,0,size);
+        System.out.println("After Sort:");
+        for(int num : arr){
+            System.out.print(num+" ");
+        }
+    }
+    public static void insertion_sort(int[] arr , int i, int n){
+        if(i == n-1){
+            return;
+        }
+        int j = i;
+        while (j > 0 && arr[j-1] > arr[j]) {
+            int temp = arr[j];
+            arr[j] = arr[j-1];
+            arr[j-1] = temp;
+        }
+        insertion_sort(arr, i+1, n);
+    }
+}
+
+//OUTPUT
+/*
+Enter the array size:5
+Enter the array elements:
+4 66 1 7 23
+After Sort:
+1 4 7 23 66
+*/
+//---------------------------------------------------------------------------------------------------------------
+
+/*Enter the array size:5
+Enter the array elements:
+4 66 1 7 23
+After Sort:
+1 4 7 23 66
+Time Complexity : O(N^2) worst & average
+                  O(N) best case
+Space Complexity : O(1)
+*/
